c92c673a05f82fa68910ac95086bb5ae95a19235
yogesh <etpr.yogesh@gmail.com>
2021-10-25 23:49:37 +0530

    index file for server and databse

---
+++ index.js
@@ -0,0 +1,38 @@
+const mongoose= require('mongoose')
+require("dotenv").config();
+
+
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+  
+mongoose.connection.on("error", (err) => {
+    console.log("Mongoose Connection ERROR: " + err.message);
+});
+  
+mongoose.connection.once("open", () => {
+    console.log("MongoDB Connected!");
+});
+//Bring in the models
+require("./models/Restaurant");
+require("./models/User");
+require("./models/Dishes");
+
+
+const app = require("./app");
+
+const server = app.listen(8000, () => {
+  console.log("Server listening on port 8000");
+});
+
+
+
+
+const jwt = require("jwt-then");
+
+//const Message = mongoose.model("Message");
+const User = mongoose.model("User");
+const List = mongoose.model("List");
+const Rating = mongoose.model("Rating");
+
